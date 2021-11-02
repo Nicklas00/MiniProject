e7575326e864c1c14c34c31add1e020c215b8b27
jona826b <jona00k3@stud.kea.dk>
2021-11-02 12:19:14 +0100

    Hjemmeside

--- wishlistv1/src/main/java/com/example/wishlistv1/controllers/FrontController.java
+++ wishlistv1/src/main/java/com/example/wishlistv1/controllers/FrontController.java
@@ -5,4 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FrontController {
 
   @GetMapping("/")
+  public String index() { return "index"; }
+
+  @GetMapping("/Oenskeliste")
+  public String oenskeliste() { return "oenskeliste"; }
+
+  @GetMapping("/blank1")
+  public String blank1() { return "blank1"; }
 }
